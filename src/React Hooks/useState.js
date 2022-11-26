4f87c9a2c595b233d71bda543f6c67d944fad5b0
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-26 19:54:54 +0530

    useState hook with initial value is integer

--- src/React Hooks/useState.js
+++ src/React Hooks/useState.js
@@ -4,10 +4,19 @@ import React, { useState } from "react";
 const UseState = () => {
   const [name, setName] = useState("john"); // Initial value is string
   const [flag, setFlag] = useState(false); // Initial value is boolean
+  const [step, setStep] = useState(0); // Initial value is integer
 
   function changeName() {
     // setName("rock");
     setFlag(!flag);
+  }
+
+  function increment() {
+    setStep(step + 1);
+  }
+
+  function decrement() {
+    setStep(step - 1);
   }
 
   return (
@@ -15,6 +24,10 @@ const UseState = () => {
       <h3>UseState</h3>
       <h4>Hello, {flag ? name : ""}</h4>
       <button onClick={changeName}>Click me</button>
+      <hr></hr>
+      <button onClick={increment}>+</button>
+      <h4>{step}</h4>
+      <button onClick={decrement}>-</button>
     </div>
   );
 };
