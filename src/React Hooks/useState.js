51eba2d036343032bc639d48c615d340073955dd
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-26 19:49:03 +0530

    useState hook with initial value is boolean

--- src/React Hooks/useState.js
+++ src/React Hooks/useState.js
@@ -3,15 +3,17 @@ import React, { useState } from "react";
 
 const UseState = () => {
   const [name, setName] = useState("john"); // Initial value is string
+  const [flag, setFlag] = useState(false); // Initial value is boolean
 
   function changeName() {
-    setName("rock");
+    // setName("rock");
+    setFlag(!flag);
   }
 
   return (
     <div>
       <h3>UseState</h3>
-      <h4>Hello, {name}</h4>
+      <h4>Hello, {flag ? name : ""}</h4>
       <button onClick={changeName}>Click me</button>
     </div>
   );
