d7b4cc0cbf962cf1df111d4788e1329e14ae8acd
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-28 12:51:17 +0530

    react lifecycle method used in class based component

--- src/React Hooks/useEffect/CComponent.js
+++ src/React Hooks/useEffect/CComponent.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CComponent extends Component {
-    state ={
-        message:"Class based Component",
-        time: new Date().toString()
-    }
-    componentDidMount(){
-        console.log("I am from componentDidMount")
-        this.interval = setInterval(this.showDate,1000)
-    }
-    componentDidUpdate(){
-        console.log("I am from componentDidUpdate")
+  state = {
+    message: "Class based Component",
+    time: new Date().toString(),
+  };
 
-    }
-    componentWillUnmount(){
-        console.log("I am from componentWillUnmount")
-        clearInterval(this.interval)
-    }
-    showDate=()=>{
-        this.setState({time: new Date().toString()})
-    }
-    render() {
-        return (
-            <div>
-                <h4>{this.state.message}</h4>
-                <h4>{this.state.time}</h4>
-            </div>
-        );
-    }
+  componentDidMount() {
+    console.log("I am from componentDidMount");
+    this.interval = setInterval(this.showDate, 1000);
+  }
+
+  componentDidUpdate() {
+    console.log("I am from componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("I am from componentWillUnmount");
+    clearInterval(this.interval);
+  }
+
+  showDate = () => {
+    this.setState({ time: new Date().toString() });
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>{this.state.message}</h4>
+        <h4>{this.state.time}</h4>
+      </div>
+    );
+  }
 }
 
 export default CComponent;
