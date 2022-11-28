b16aee1a96b3311a516ca7def90fd1e3d780ec30
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-28 13:30:02 +0530

    functional based component with two state for showing time and message

--- src/React Hooks/useEffect/CComponent.js
+++ src/React Hooks/useEffect/CComponent.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class CComponent extends Component {
-  state = {
-    message: "Class based Component",
-    time: new Date().toString(),
-  };
+    state ={
+        message:"Class based Component",
+        time: new Date().toString()
+    }
 
-  componentDidMount() {
-    console.log("I am from componentDidMount");
-    this.interval = setInterval(this.showDate, 1000);
-  }
+    componentDidMount(){
+        console.log("I am from componentDidMount")
+        this.interval = setInterval(this.showDate,1000)
+    }
+    
+    componentDidUpdate(){
+        console.log("I am from componentDidUpdate")
+    }
 
-  componentDidUpdate() {
-    console.log("I am from componentDidUpdate");
-  }
+    componentWillUnmount(){
+        console.log("I am from componentWillUnmount")
+        clearInterval(this.interval)
+    }
 
-  componentWillUnmount() {
-    console.log("I am from componentWillUnmount");
-    clearInterval(this.interval);
-  }
+    showDate=()=>{
+        this.setState({time: new Date().toString()})
+    }
 
-  showDate = () => {
-    this.setState({ time: new Date().toString() });
-  };
-
-  render() {
-    return (
-      <div>
-        <h4>{this.state.message}</h4>
-        <h4>{this.state.time}</h4>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h4>{this.state.message}</h4>
+                <h4>{this.state.time}</h4>
+            </div>
+        );
+    }
 }
 
 export default CComponent;
--- src/React Hooks/useEffect/FComponent.js
+++ src/React Hooks/useEffect/FComponent.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function FComponent(props) {
-    return (
-        <div>
-            <h4>FComponent</h4>
-        </div>
-    );
-}
+const FComponent = () => {
+  const [message, setMessage] = useState("Functional based Component");
+  const [time, setTime] = useState("");
+
+  const showDate = () => {
+    setTime(new Date().toString());
+  };
+  
+  return (
+    <div>
+      <h4>{message}</h4>
+      <h4>{time}</h4>
+      <button onClick={showDate}>Show Date and Time</button>
+      <button onClick={() => setMessage("Functional Component")}>
+        Change Message
+      </button>
+    </div>
+  );
+};
 
 export default FComponent;
