7f2d54129e4d8ecdf0889bd16940d3e673a82880
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-28 12:47:52 +0530

    react lifecycle method used

--- src/React Hooks/useEffect/CComponent.js
+++ src/React Hooks/useEffect/CComponent.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function CComponent(props) {
-    return (
-        <div>
-            <h4>CComponent</h4>
-        </div>
-    );
+class CComponent extends Component {
+    state ={
+        message:"Class based Component",
+        time: new Date().toString()
+    }
+    componentDidMount(){
+        console.log("I am from componentDidMount")
+        this.interval = setInterval(this.showDate,1000)
+    }
+    componentDidUpdate(){
+        console.log("I am from componentDidUpdate")
+
+    }
+    componentWillUnmount(){
+        console.log("I am from componentWillUnmount")
+        clearInterval(this.interval)
+    }
+    showDate=()=>{
+        this.setState({time: new Date().toString()})
+    }
+    render() {
+        return (
+            <div>
+                <h4>{this.state.message}</h4>
+                <h4>{this.state.time}</h4>
+            </div>
+        );
+    }
 }
 
 export default CComponent;
