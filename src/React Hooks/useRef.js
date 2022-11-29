bf8a22bf5ed4fb96ae4491ad7f7e857c77519b77
Tausif4171 <tausifkhan4171@gmail.com>
2022-11-29 13:42:58 +0530

    UseRef for storing the previous state

--- src/React Hooks/useRef.js
+++ src/React Hooks/useRef.js
@@ -1,24 +1,35 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function UseRef() {
   const [name, setName] = useState("");
   const [counter, setCounter] = useState(0);
-  // DOM Reference
-  const inputE1 = useRef();
-  console.log(inputE1.current);
+
+  // DOM Reference - Using useRef we can manipulate the DOM or access the DOM elements.
+  const inputE1Ref = useRef();
+  console.log(inputE1Ref.current);
+
+  // UseRef for storing the previous state
+  // Hold mutable value prevent re-render of component
+  const previousCounterRef = useRef();
+  console.log(previousCounterRef.current);
 
   const resetInput = () => {
     setName("");
-    inputE1.current.focus();
-    console.log(inputE1.current.value);
-    inputE1.current.value = "pqr"; // we can't do like this if we want to update used useState Hook
+    inputE1Ref.current.focus();
+    console.log(inputE1Ref.current.value);
+    inputE1Ref.current.value = "pqr"; // we can't do like this if we want to update used useState Hook
   };
+
+  useEffect(() => {
+    // console.log({counter})
+    previousCounterRef.current = counter;
+  }, [counter]);
 
   return (
     <div>
       <h3>useRef ☑️</h3>
       <input
-        ref={inputE1}
+        ref={inputE1Ref}
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
@@ -26,6 +37,7 @@ function UseRef() {
       <h4>My name is {name}</h4>
       <div>
         <h4>Counter is: {counter}</h4>
+        <p>Previous Counter is: {previousCounterRef.current}</p>
         <button onClick={() => setCounter(Math.ceil(Math.random() * 100))}>
           Generate Number
         </button>
