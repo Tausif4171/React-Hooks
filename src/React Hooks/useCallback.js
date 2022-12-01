adc367106a7851d449cbce90eb695fbfe3edb20e
Tausif4171 <tausifkhan4171@gmail.com>
2022-12-01 13:12:17 +0530

    finding factorial number of counter values

--- src/React Hooks/useCallback.js
+++ src/React Hooks/useCallback.js
@@ -3,11 +3,22 @@ import React, { useState } from "react";
 const UseCallback = () => {
   const [counter, setCounter] = useState(1);
   const [name, setName] = useState("");
-  
+  const result = factorial(counter)
+  function factorial(n){
+    if(n<0){
+        return -1
+    }
+    if(n===0){
+        return 1
+    }
+    else{
+        return n*factorial(n-1)
+    }
+  }
   return (
     <div>
       <h3>UseCallback ☑️</h3>
-      <h4>{counter}</h4>
+      <h4>Factorial of {counter} is {result}</h4>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
       <button onClick={() => setCounter(counter - 1)}>Decrement</button>
       <h4>Enter Name:</h4>
