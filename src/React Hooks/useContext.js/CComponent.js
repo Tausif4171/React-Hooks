cb1caf4faf94cec8e53f0ef10273a1c43c6e9347
Tausif4171 <tausifkhan4171@gmail.com>
2022-12-03 14:26:11 +0530

    passing counter value as a props to child functional component

--- src/React Hooks/useContext.js/CComponent.js
+++ src/React Hooks/useContext.js/CComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 class CComponent extends React.Component {
   render() {
     return (
-      <div style={{ border: "2px solid #000", margin: "0px 120px 0px 120px" }}>
+      <div style={{ border: "2px solid grey", margin: "0px 120px 0px 120px" }}>
         <h3>Class Component</h3>
         <h4>{this.props.counter}</h4>
       </div>
--- src/React Hooks/useContext.js/FComponent.js
+++ src/React Hooks/useContext.js/FComponent.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const FComponent = (pros) => {
-  const { counter } = pros;
+const FComponent = (props) => {
+  const { counter } = props;
   return (
-    <div style={{ border: "2px solid #000", margin: "20px 120px 20px 120px" }}>
+    <div style={{ border: "2px solid grey", margin: "20px 120px 20px 120px" }}>
       <h3>Functional Component</h3>
+      <h4>{counter}</h4>
+      <FComponentChild counter={counter} />
+    </div>
+  );
+};
+
+const FComponentChild = ({counter}) => {
+  return (
+    <div style={{ border: "2px solid grey", margin: "20px 120px 20px 120px" }}>
+      <h3>Functional Component Child</h3>
       <h4>{counter}</h4>
     </div>
   );
